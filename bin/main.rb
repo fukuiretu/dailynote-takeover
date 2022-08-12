0e5f046fd2591544ffdcfc19cd1db3da470ce1f8
fukuiretu <fukuiretu@gmail.com>
2022-08-13 00:18:57 +0900

    refactor

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "../lib/dailynote"
 require_relative "../lib/dropbox"
 
@@ -6,7 +8,7 @@ class Main
     dailynote_dir = ENV['DAILYNOTE_DIR']
     output_path = Dailynote::Make.call(dailynote_dir)
 
-    # TODO: フェーズ2ではGitHub Action越しに実行するのでDropboxのAPI経由に改修予定
+    # TODO:フェーズ2ではGitHub Action越しに実行するのでDropboxのAPI経由に改修予定
     File.write("#{dailynote_dir}/#{Time.now.strftime('%Y-%m-%d')}.md", File.read(output_path))
 
     # Dropbox::Download.call
--- lib/dropbox.rb
+++ lib/dropbox.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative './dropbox/download'
 require_relative './dropbox/upload'
 
